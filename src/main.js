403b879f68cc1d95bfada2a9bdfcafa56fa491d8
jlztan <zhou_jinlei@163.com>
2020-06-18 16:14:15 +0800

    add element-ui

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import store from './store';
 import dragula from 'dragula';
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
 Vue.use(dragula);
 Vue.use(ViewUI);
+Vue.use(ElementUI);
 
 Vue.prototype.dragula = dragula;
 
