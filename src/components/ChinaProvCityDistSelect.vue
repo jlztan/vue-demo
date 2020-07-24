fc12f4b7eaf5670faf254704694a6f409df19b8a
jlztan <zhou_jinlei@163.com>
2020-07-24 12:41:37 +0800

    改进省-市-区联动 demo

--- src/components/ChinaProvCityDistSelect.vue
+++ src/components/ChinaProvCityDistSelect.vue
@@ -2,20 +2,20 @@
   <div class="wrapper">
     <!-- 省份选择 -->
     <select id="prov" @change="showCity($event)">
-      <option>=请选择省份=</option>
+      <option value="-1">=请选择省份=</option>
     </select>
 
     <!--城市选择-->
     <select id="city" @change="showCountry($event)">
-      <option>=请选择城市=</option>
+      <option value="-1">=请选择城市=</option>
     </select>
 
-    <!--县区选择-->
-    <select id="country" @change="selecCountry($event)">
-      <option>=请选择区县=</option>
+    <!--区县选择-->
+    <select id="county" @change="selecCountry($event)">
+      <option value="-1">=请选择区县=</option>
     </select>
 
-    <button type="button" class="btn met1" @click="showAddr()">确定</button>
+    <button type="button" class="btn" @click="showAddr()">确定</button>
   </div>
 </template>
 
@@ -28,103 +28,92 @@ export default {
       current: {
         prov: '',
         city: '',
-        country: ''
+        county: ''
       },
       btn: '',
       prov: '',
       city: '',
-      country: '',
-      res: ''
+      county: ''
     };
   },
   mounted() {
-    this.btn = document.getElementsByClassName('met1')[0];
+    this.btn = document.querySelector('.btn');
     this.prov = this.getElement('prov');
     this.city = this.getElement('city');
-    this.country = this.getElement('country');
+    this.county = this.getElement('county');
     this.showProv();
   },
   methods: {
     /*根据所选的省份来显示城市列表*/
     showCity(event) {
-      let obj = event.target;
-      var val = obj.options[obj.selectedIndex].value;
-      console.log(val);
-      if (val >= 0) {
+      let selectObj = event.target;
+      let value = selectObj.options[selectObj.selectedIndex].value;
+      // 选择其他省，禁用确定按钮
+      this.current.prov = value;
+      this.btn.disabled = true;
+      if (value >= 0) {
         this.city.style.display = 'inline-block';
-        this.country.style.display = 'none';
-      } else {
-        this.city.style.display = 'none';
-        this.country.style.display = 'none';
-      }
-      if (val != this.current.prov) {
-        // 选择其他省，清空输入框，禁用确定按钮
-        this.current.prov = val;
-        this.btn.disabled = true;
-      }
-      if (val != null) {
-        // 填充市下拉选
-        this.city.length = 1;
-        if (provinces[val]) {
-          var cityLen = provinces[val]['city'].length;
-        }
-        for (var j = 0; j < cityLen; j++) {
-          var cityOpt = document.createElement('option');
-          cityOpt.innerText = provinces[val]['city'][j].name;
-          cityOpt.value = j;
+        this.county.style.display = 'none';
+
+        this.city.length = 1; // 清空城市下拉选，只保留第一个默认选项
+        let cityLen = provinces[value]['city'].length;
+        // 填充市下拉选;
+        for (let i = 0; i < cityLen; i++) {
+          let cityOpt = document.createElement('option');
+          cityOpt.innerText = provinces[value]['city'][i].name;
+          cityOpt.value = i;
           this.city.appendChild(cityOpt);
         }
+      } else {
+        this.city.style.display = 'none';
+        this.county.style.display = 'none';
       }
     },
 
-    /*根据所选的城市来显示县区列表*/
+    /*根据所选的城市来显示区县列表*/
     showCountry(event) {
-      let obj = event.target;
-      var val = obj.options[obj.selectedIndex].value;
-      if (val >= 0) {
-        this.country.style.display = 'inline-block';
-      } else {
-        this.country.style.display = 'none';
-      }
-      this.current.city = val;
-      if (val != null) {
-        this.country.length = 1; //清空之前的内容只留第一个默认选项
-        if (provinces[this.current.prov]['city'][val]) {
-          var countryLen = provinces[this.current.prov]['city'][val].districtAndCounty.length;
-        }
-        if (countryLen == 0) {
-          // 没有县的情况下，直接在输入框显示 省-市
+      let selectObj = event.target;
+      let value = selectObj.options[selectObj.selectedIndex].value;
+      this.current.city = value;
+      this.btn.disabled = true;
+      if (value >= 0) {
+        this.county.style.display = 'inline-block';
 
-          return;
+        this.county.length = 1;
+        let countryLen = provinces[this.current.prov]['city'][value].districtAndCounty.length;
+        for (let i = 0; i < countryLen; i++) {
+          let countryOpt = document.createElement('option');
+          countryOpt.innerText = provinces[this.current.prov]['city'][value].districtAndCounty[i];
+          countryOpt.value = i;
+          this.county.appendChild(countryOpt);
         }
-        for (var n = 0; n < countryLen; n++) {
-          var countryOpt = document.createElement('option');
-          countryOpt.innerText = provinces[this.current.prov]['city'][val].districtAndCounty[n];
-          countryOpt.value = n;
-          this.country.appendChild(countryOpt);
-        }
+      } else {
+        this.county.style.display = 'none';
       }
     },
 
     /*选择县区之后的处理函数*/
     selecCountry(event) {
-      let obj = event.target;
-      this.current.country = obj.options[obj.selectedIndex].value;
-      if (this.current.city != null && this.current.country != null) {
+      this.btn.disabled = true;
+      let selectObj = event.target;
+      let value = selectObj.options[selectObj.selectedIndex].value;
+      this.current.county = value;
+      if (value < 0) {
+        this.btn.disabled = true;
+      } else {
         this.btn.disabled = false;
       }
     },
 
     /*点击确定按钮显示用户所选的地址*/
     showAddr() {
-      this.res =
-        provinces[this.current.prov].name +
-        '-' +
-        provinces[this.current.prov]['city'][this.current.city].name +
-        '-' +
-        provinces[this.current.prov]['city'][this.current.city].districtAndCounty[this.current.country];
-      console.log(this.res);
-      this.$emit('confirm', this.res);
+      let res = '';
+      let province = provinces[this.current.prov].name;
+      let city = provinces[this.current.prov]['city'][this.current.city].name;
+      let county = provinces[this.current.prov]['city'][this.current.city].districtAndCounty[this.current.county];
+      res = province + '-' + city + '-' + county;
+      console.log(res);
+      this.$emit('confirm', res);
     },
     getElement(str) {
       return document.getElementById(str);
@@ -185,7 +174,7 @@ select#city {
   display: none;
 }
 
-select#country {
+select#county {
   display: none;
 }
 </style>
